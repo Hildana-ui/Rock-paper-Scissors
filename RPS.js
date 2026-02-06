771e7cbba46ed9c9eebf6efa120f87b2b835feac
Hildana-ui <hildana.amare@bitscollege.edu.et>
2026-02-06 12:26:38 +0300

    modified code to add ui

--- RPS.js
+++ RPS.js
@@ -1,28 +1,40 @@
-let humanScore = 0
-let computerScore = 0
+let humanScore = 0;
+let computerScore = 0;
+
+
+const div = document.createElement('div');
+document.body.appendChild(div);
+
+
+
+const rock = document.createElement('button');
+const paper = document.createElement('button');
+const scissor = document.createElement('button');
+
+
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissor.textContent = "Scissor";
+
+
+document.body.appendChild(rock);
+document.body.appendChild(paper);
+document.body.appendChild(scissor);
+
+
 
 function getComputerChoice() {
-    const num = Math.floor(Math.random() * 3) + 1;
-    if (num == 1) {
-        return "rock";
-    } else if (num == 2) {
-        return "paper";
-    } else {
-        return "scissor";
-    } 
+    const num = Math.floor(Math.random() * 3);
+    return ["rock","paper","scissor"][num];
 }
 
 
+function playRound(humanChoice) {
 
-function getHumanChoice() {
-    let choice = prompt("Enter: ").toLowerCase();
-    return choice;
-}
+    const computerChoice = getComputerChoice();
 
-
-function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log("It's a tie!");
+        div.textContent = "It's a tie!";
         return;
     }
 
@@ -31,36 +43,56 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice === "paper"   && computerChoice === "rock")    ||
         (humanChoice === "scissor" && computerChoice === "paper")
     ) {
-        console.log(`You WIN! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`);
-        return "human";
+        div.textContent = `You WIN! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`;
+        humanScore++;
     } else {
-        console.log(`You LOSE! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}`);
-        return "computer";
+        div.textContent = `You LOSE! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}`;
+        computerScore++;
     }
+
+    div.textContent += ` | You: ${humanScore} Computer: ${computerScore}`;
 }
 
 
+rock.addEventListener("click", () => {
+    playRound("rock");
+});
+
+paper.addEventListener("click", () => {
+    playRound("paper");
+});
+
+scissor.addEventListener("click", () => {
+    playRound("scissor");
+});
 
 
 
-function playGame() {
-    for (i = 1; i <= 5; i++) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
+// function playGame(humanChoice) {
+//     const hs = document.createElement('div');
+//     const cs = document.createElement('div');
 
-        const Score = playRound(humanChoice, computerChoice);
+//     for (let i = 1; i <= 5; i++) {
+//         const computerChoice = getComputerChoice();
 
-        if (Score === "human") humanScore++;
-        if (Score === "computer") computerScore++;
+//         const Score = playRound(humanChoice, computerChoice);
+
+//         if (Score === "human") humanScore++;
+//         if (Score === "computer") computerScore++;
 
 
-        console.log(`Your score: ${humanScore}`);
-        console.log(`Computer Score: ${computerScore}`);
-    }   
+//         hs.textContent = `Your score: ${humanScore}`;
+//         cs.textContent = `Computer Score: ${computerScore}`;
+//     }  
+
     
-    console.log("Final Score:");
-    console.log("You: ", humanScore);
-    console.log("Computer: ", computerScore)
-}
+//     const fs = document.createElement('div');
+//     const fhs = document.createElement('div');
+//     const fcs = document.createElement('div');
+    
+//     fs.textContent = "Final Score:";
+//     fhs.textContent = `You: ${humanScore}`;
+//     fcs.textContent = `Computer: ${computerScore}`;
+// }
 
-playGame();
+// playGame();
